a82665130086a4fdf629755385d5c5d56e812161
George Krotov <zvuk24@gmail.com>
2021-11-09 20:56:25 +0700

    1

---
+++ Задание 5/Задание 5-3а/Задание 5-3а.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int res = 0; // результат будет целым
+    int i = 0; // задаем счетчик цикла
+    while (i <= 1000) {
+        res += i;
+        i++;
+    }
+    cout << "Сумма всех целых чисел от 1 до 1000 включительно равна " << res << endl;
+}
